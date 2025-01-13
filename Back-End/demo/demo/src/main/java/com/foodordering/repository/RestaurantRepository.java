44f180d7ab1d1c48fa3bd4242b04e4e29eb58803
Swapnil Shinde <shindeswapnil1101@gmail.com>
2025-01-13 17:53:18 +0530

    add MessageResponse,RestaurantController

--- Back-End/demo/demo/src/main/java/com/foodordering/repository/RestaurantRepository.java
+++ Back-End/demo/demo/src/main/java/com/foodordering/repository/RestaurantRepository.java
@@ -6,14 +6,20 @@ import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfig
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.foodordering.model.Restaurant;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 	
-	@Query("SELECT r FROM Restaurant R WHERE lower (r.name)LIKE lower (concat('%',:query, '%'))"
-			+ "OR lower(r.cuisineType)LIKE lower(concat('%',query,'%'))")
-	List<Restaurant> findBySearchQuery(String query);
+//	@Query("SELECT r FROM Restaurant R WHERE lower (r.name)LIKE lower (concat('%',:query, '%'))"
+//			+ "OR lower(r.cuisineType)LIKE lower(concat('%',query,'%'))")
+//	List<Restaurant> findBySearchQuery(String query);
+	
+	// Custom query for search
+	
+    @Query("SELECT r FROM Restaurant r WHERE r.name LIKE %:keyword% OR r.description LIKE %:keyword%")
+    List<Restaurant> findBySearchQuery(@Param ("keyword") String keyword);
 	
 	Restaurant findByOwnerId(Long userId);
 
